f672e740aa7ac9a795ab99d137db445a04535184
jack96489 <giacomo.orsenigo@gmail.com>
2019-03-20 18:41:41 +0100

    Aggiunta demo LAN

---
+++ DemoLAN/Assets/Vanguard/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Inizializzo l'animazione
+        //anim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            Debug.Log("Schiacciato salta");
+            anim.CrossFade("Jump", 0.2f);
+        }
+        else
+        {
+            //anim.SetBool("jump", false);
+        }
+    }
+}
