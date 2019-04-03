35964b781dbe0d5c8df38c4160b178c3c0e313ca
Samuele Peduzzi <43852643+Curiosity48@users.noreply.github.com>
2019-04-03 17:15:47 +0200

    Prova script flag

---
+++ Flag Demo/Assets/MyGameObjects/MyFlag/Flag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    private bool isFlagCaptured;
+
+    void Start()
+    {
+        isFlagCaptured = false;
+        Debug.Log("Start");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        isFlagCaptured = true;
+        Debug.Log("Bandiera catturata !!");
+
+        //TO DO
+    }
+}
