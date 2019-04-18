8f71bdee46c84d084b20022752a4972fa73c0b72
TheFedelino01 <federico.saccani01@gmail.com>
2019-04-18 22:32:46 +0200

    Realizzato il Menu HUD per i client
    +Aggiunti 2 bottoni per hostare la partita oppure per entrare in una partita gia' esistente
    
    +Aggiunti i bottoni "opzioni" ed "Esci"
    
    +Aggiunta schermata "Home" e "Informazioni" con le informazioni del client come IP e nome utente
    
    +Aggiunte le immagini e le animazioni per una grafica migliore (Anche logo scuola e link al sito del progetto e al sito jm)
    
    -Manca ora tutto il relativo funzionamento quindi una volta premuto qualsiasi pulsante, far partire la funzione corrispondente

---
+++ HUD-only/Assets/Standard Assets/Characters/FirstPersonCharacter/Prefabs/login.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class login : MonoBehaviour {
+
+    public Texture2D menuImage;
+    public Texture2D mirino;
+    static public int punti = 0;
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(10, 10, 100, 20), menuImage); //new Rect(10, 10, menuImage.width, menuImage.height)
+        GUI.Window(1, new Rect(10, 50, 100, 150), menuWindow, "Menù");
+        GUI.DrawTexture(new Rect(Screen.width / 2f, Screen.height / 2f,
+                        10f, 10f), mirino);
+    }
+    void menuWindow(int id)
+    {
+        if (GUILayout.Button("TEST"))
+            Debug.Log("Hai premuto il pulsante TEST!");
+        GUILayout.Label("Punti: " + punti);
+        GUILayout.Label("Vite: infinite");
+
+        if (GUILayout.Button("ESCI"))
+            Application.Quit();
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HUD-only/Assets/Standard Assets/Characters/FirstPersonCharacter/Prefabs/spara.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spara : MonoBehaviour
+{
+
+    public GameObject prefabProiettile;
+    public float speed = 6000f;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            //Lo sparò verrà originato nello SpawnPoint             
+            // che va messo dentro la Cam, altrimenti non rileva le inclinazioni su/giu 
+
+            GameObject spawnPoint = GameObject.Find("SpawnPoint");
+            //new GameObject(...)
+            //Il proiettile generato
+            GameObject nuovoPr = (GameObject)Instantiate(prefabProiettile,
+                spawnPoint.transform.position, this.transform.rotation);
+
+            //Aggiungo alcune caratteristiche al proiettile
+            nuovoPr.GetComponent<Rigidbody>().AddForce(transform.forward * speed,
+                ForceMode.Impulse);
+
+            Debug.Log("Sparato");
+            shoot();
+        }
+
+    }
+
+
+    private void shoot()
+    {
+        Camera cam = Camera.main;//Prendo la camera FPS
+        //Prendo la linea in mezzo allo schermo
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        RaycastHit hit;
+
+        //Controllo se colpisco qualcosa
+        if(Physics.Raycast(ray,out hit))
+        {
+            //Vedo se è un nemico
+            if (hit.collider.tag.ToUpper() == "PlayerREMOTE")
+            {
+                Destroy(hit.collider.gameObject);
+                login.punti++;
+
+                //Posiziono l'esplosione sulle cordinate dell'oggetto colpito
+                GameObject esplosione = (GameObject)Instantiate(explosion,
+                hit.collider.gameObject.transform.position, hit.collider.gameObject.transform.rotation);
+
+            }
+        }
+    }
+
+
+}
