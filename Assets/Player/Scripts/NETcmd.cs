5f74bc73994b2e9d42258defbe4ff11b125f949a
TheFedelino01 <federico.saccani01@gmail.com>
2019-04-20 13:04:25 +0200

    Aggiunta funzionalita' JOIN A SERVER
    +Quando viene premuto il pulsante, appare un menu con un animazione che permette di impostare l'indirizzo IP a cui si vuole collegarsi.
    
    +Il campo per l'IP ha il controllo utilizzando la classe (IPAddress)
    Se e' stato inserito un indirizzo corretto, cliccando il pulsante "connetti" iniziera' la connessione. Se l'indirizzo e' corretto ma non riesce a collegarsi, apparira' il pulsante "cancella" e se schiacciato, annullera' il processo di connessione.
    
    -Avevo provato a farlo con una lista dove erano presenti gli indirizzo IP dei server che trovava ma da quando ho capito, non é possibile capire che server sono disponibili ne tantomento realizzare una lista a schermo che funziona.

--- Assets/Player/Scripts/NETcmd.cs
+++ Assets/Player/Scripts/NETcmd.cs
@@ -1,31 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NETcmd : MonoBehaviour
 {
+    private int port = 7777;
+    private string ip = "";
+    public InputField inputIP;
+    private bool connecting = false;
+
+    public GameObject stopConnecting;
+    public GameObject connect;
+    public GameObject labelConnecting;
+
     // Start is called before the first frame update
     void Start()
     {
+        stopConnecting.SetActive(false);
+        connect.SetActive(true);
 
+        labelConnecting.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Sta provando a connettersi
+        if (connecting == true)
+        {
+            //Mostro il bottone cancella connessione se non si e' connesso
+            if (NetworkManager.singleton.IsClientConnected() == false)
+            {
+                labelConnecting.SetActive(true);
+                stopConnecting.SetActive(true);//visualizzo il bottone cancella
 
+                connect.SetActive(false);//Nascondo il bottone connetti
+            }
+            else
+            {
+                connecting = false;
+                stopConnecting.SetActive(false);//Nascondo il bottone cancella
+                connect.SetActive(true);//visualizzo il bottone connetti
+            }
+        }
     }
+
+
+
+    public void stopTryingToJoin()
+    {
+        connecting = false;//Dico che non sto piu cercando di connettermi
+
+        labelConnecting.SetActive(false);
+        stopConnecting.SetActive(false);//Nascondo il bottone cancella
+
+        connect.SetActive(true);//visualizzo il bottone connetti
+
+        NetworkManager.singleton.client.Disconnect();//Disconnetto il client
+    }
+
+
 
     public void serverStartMIO()
     {
-        Debug.Log("STARTING SERVCER...");
-        NetworkManager.singleton.networkPort = int.Parse("7777");
+        Debug.Log("STARTING SERVER...");
+        NetworkManager.singleton.networkPort = port;
         NetworkManager.singleton.StartHost();
 
         //imposto che sta giocando
         ///TODO Impostarlo anche quando un client si connete ad un server
         GameManager.instance.partitaAvviata = true;
-        Debug.Log("Partita avviata " + GameManager.instance.partitaAvviata+ GameManager.instance.partitaAvviata);
+        Debug.Log("Partita avviata " + GameManager.instance.partitaAvviata + GameManager.instance.partitaAvviata);
     }
+
+    public void connectTo()
+    {
+        IPAddress x;
+        //Controllo se e' stato inserito un IP esistente
+        if (IPAddress.TryParse(ip, out x))
+        {
+            connettitiAspettando(0.3f);//Aspetto cosí riproduce il suono del bottone
+        }
+        else
+        {
+            Debug.Log("IP inesistente!: " + ip);
+        }
+    }
+
+    public void IPCHANGED()
+    {
+        ip = inputIP.text;//Prendo il testo inserito
+        Debug.Log("Nuovo IP: " + ip);
+    }
+
+    private void connettitiAspettando(float sec) {
+        StartCoroutine(cnt(sec));
+    }
+
+    private IEnumerator cnt(float sec)
+    {
+        //Aspetto il tempo indicato
+        yield return new WaitForSeconds(sec);
+
+        Debug.Log("IP attendibile: " + ip);
+        Debug.Log("Connessione in corso...");
+        //Imposto l'ip e la porta dove collegarsi
+        NetworkManager.singleton.networkAddress = ip;
+        NetworkManager.singleton.networkPort = port;
+
+        //Lo faccio collegare
+        NetworkManager.singleton.StartClient();
+        connecting = true;
+    }
+
 }
+
+
+
+
