f41823522fc474f63d8803b15a2fac24f22ab503
Samuele Peduzzi <43852643+Curiosity48@users.noreply.github.com>
2019-04-20 19:44:28 +0200

    Correzione errori flagDemo Intr. controllo spawn

--- Assets/Flag/MyFlag/Flag.cs
+++ Assets/Flag/MyFlag/Flag.cs
@@ -7,14 +7,21 @@ public class Flag : MonoBehaviour
     public bool isFlagCaptured { get; set; }
     public bool isFlagConquered { get; set; }
 
-    private Collision collisionPlayer;
+    private string collisionPlayerName;
+
+    private GameObject spawnPoint1, spawnPoint2;
+
+
+    private Transform getPlayerTransform()
+    {
+        return (GameManager.getPlayer(collisionPlayerName)).transform; 
+    }
 
 
     private void followThePlayer()
     {
-        //TO DO posizione player da aggiornare !!
-        this.transform.position = GameManager.getPlayer(collisionPlayer.collider.name).transform.position;
-        Debug.Log("Sto seguendo il player ...");
+        Debug.Log("La bandiera " + this.name + " è nelle mani di: " + collisionPlayerName + " Posizione -->" + getPlayerTransform().position.ToString());
+        //la bandiera per motivi di performance non segue il player
     }
 
     private void conqueredFlag()
@@ -32,19 +39,29 @@ public class Flag : MonoBehaviour
 
     private void chkPlayerPosition()
     {
-        Debug.Log("Sto controllando la posizione...");
+        //Debug.Log(this.name + ": Sto controllando la posizione...");
         //Controlla la posizione del player che si è impossessato della bandiera 
         //Se quest'ultimo giunge allo spawn d'attacco la bandiera è conquistata
         //if(player.Position == spawn)
-            //conqueredFlag();
+        //conqueredFlag();
+        //if (getPlayerTransform().position == spawnPoint1.transform.position
+        //    || getPlayerTransform().position == spawnPoint2.transform.position)
+        //{
+        //    isFlagCaptured = false;
+        //    isFlagConquered = true;
+        //    Debug.Log("La bandiera " + this.name + "è stata conquistata da " + collisionPlayerName);
+        //}
+
+
     }
 
 
     private void flagCaptured(Collision other)
     {
         isFlagCaptured = true;
-        collisionPlayer = other;
-        Debug.Log("Flag captured !!");
+        collisionPlayerName = other.collider.name;
+
+        Debug.Log(this.name + ": Flag captured !!");
     }
 
 
@@ -56,7 +73,9 @@ public class Flag : MonoBehaviour
     void Start()
     {
         isFlagCaptured = false;
-        Debug.Log("Start");
+        spawnPoint1 = GameObject.Find("spawn1");
+        spawnPoint1 = GameObject.Find("spawn2");
+        Debug.Log(this.name + ": Start");
     }
 
     void Update()
