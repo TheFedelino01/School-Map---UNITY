16799ae9a7ef3f886e665eb95271e1124c2be8f6
jack96489 <giacomo.orsenigo@gmail.com>
2019-05-10 15:58:41 +0200

    Aggiunto pannello chat

---
+++ Assets/Scripts/chatManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chatManager : MonoBehaviour
+{
+    public GameObject chatPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.partitaAvviata)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+                chatPanel.SetActive(!chatPanel.active);
+        }
+        else if (chatPanel.active)
+            chatPanel.SetActive(false);
+    }
+}
